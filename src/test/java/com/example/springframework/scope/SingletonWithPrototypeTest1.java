b9219898b160b14edc8999e8f409884164cd9e6c
jisupark-dev <dodlal123tkd@gmail.com>
2023-07-25 14:37:47 +0900

    [commit] singleton과 prototype bean을 함께 사용했을 때 발생하는 문제점 -> singleton생성시 prototype빈이 한번만 주입받아서, 사용자마다 다르게 반응하지 못하게 된다.

---
+++ src/test/java/com/example/springframework/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,94 @@
+package com.example.springframework.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SingletonWithPrototypeTest1 {
+    @Test
+    void prototypeFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+
+        bean1.addCount();
+        System.out.println(bean1.getCount());
+        assertThat(bean1.getCount()).isEqualTo(1);
+        bean2.addCount();
+        System.out.println(bean2.getCount());
+        assertThat(bean2.getCount()).isEqualTo(1);
+
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(
+                        ClientBean2.class, PrototypeBean.class
+                );
+
+        ClientBean2 clientBean1 = ac.getBean(ClientBean2.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+        ClientBean2 clientBean2 = ac.getBean(ClientBean2.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean; // 생성 시점에 주입됨.
+
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+    @Scope("singleton")
+    static class ClientBean2 {
+        @Autowired
+        ApplicationContext applicationContext;
+
+        public int logic() {
+            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("prototypeBean.init"+this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("prototypeBean.destroy");
+        }
+    }
+}
